36fa7a0f2a217fb9a41662fe1f9d4cb7901c458d
Cliff <wyclifflumumba@gmail.com>
2020-11-03 17:28:54 -0600

    Updated git ignore.

--- src/chaosApplication/Main.java
+++ src/chaosApplication/Main.java
@@ -12,8 +12,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
+
+
 
     }
 
--- src/chaosApplication/mainController.java
+++ src/chaosApplication/mainController.java
@@ -1,5 +1,31 @@
 package chaosApplication;
 
-public class mainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable
+{
+    @FXML
+    private HBox hBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        //HBox hbox = new HBox();
+        Button button1 = new Button("Add");
+        Button button2 = new Button("Remove");
+        hBox.setHgrow(button1, Priority.ALWAYS);
+        hBox.setHgrow(button2, Priority.ALWAYS);
+        button1.setMaxWidth(Double.MAX_VALUE);
+        button2.setMaxWidth(Double.MAX_VALUE);
+        hBox.getChildren().addAll(button1, button2);
+    }
 }
+
+
