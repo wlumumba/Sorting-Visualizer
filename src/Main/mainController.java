bcfebd312542e28ff8e2ede65a1fba8b3665db65
Moussa Dastgheib <moses@mdast.me>
2020-11-04 14:38:15 -0600

    Setup a slider in fxml that will auto update a label text. (We can use this to pull array sizes with getText();. Added generic MergeSort

--- src/Main/mainController.java
+++ src/Main/mainController.java
@@ -3,29 +3,40 @@ package Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class mainController implements Initializable
+public class mainController
 {
     @FXML
-    private HBox hBox;
+    private Slider slider;
+    @FXML
+    private Label label;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources)
+    public void onSliderChanged() {
+        //Using onMouseRelease FXML::id defined label, setting the value of slider to the corresponding text box (see fxml for more)
+    label.setText(slider.getValue()+" ");
+}
+
+    /*@Override
+    //public void initialize(URL location, ResourceBundle resources)
     {
-        //HBox hbox = new HBox();
+       /* //HBox hbox = new HBox();
         Button button1 = new Button("Add");
         Button button2 = new Button("Remove");
         hBox.setHgrow(button1, Priority.ALWAYS);
         hBox.setHgrow(button2, Priority.ALWAYS);
         button1.setMaxWidth(Double.MAX_VALUE);
         button2.setMaxWidth(Double.MAX_VALUE);
-        hBox.getChildren().addAll(button1, button2);
+        hBox.getChildren().addAll(button1, button2);*/
     }
-}
+
+
 
 
