024ad2471641b69ded5da5e4825593abeb8fc1cd
Cliff <wyclifflumumba@gmail.com>
2020-11-03 23:32:46 -0600

    Changed dimensions of main screen.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Sorting Visualizer");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
