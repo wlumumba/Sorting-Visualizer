6267856f094daa72302314abe86df0bf20516104
Moussa Dastgheib <moses@mdast.me>
2020-11-10 13:14:33 -0600

    Adjusted choice box default, removed merge sort for now.

--- src/Main/mainController.java
+++ src/Main/mainController.java
@@ -64,7 +64,10 @@ public class mainController implements Initializable
         hBox.setAlignment(Pos.TOP_CENTER);
         rects = Model.generateRandomRects();
         hBox.getChildren().addAll(rects);
+        //Setting the choice box sort list
         sortChoiceBox.setItems(FXCollections.observableArrayList("Merge Sort", "Bubble Sort", "Moses"));
+        //Setting default value of the choice box
+        sortChoiceBox.setValue("Bubble Sort");
 
         ///Testing looping over rectangles
         /*
@@ -129,7 +132,6 @@ public class mainController implements Initializable
 
             SequentialTransition sq = new SequentialTransition();
             sq.getChildren().addAll(start.swap(rects));
-            sq.play();
 
             System.out.println(rects);
             sizeSlider.setDisable(false);
