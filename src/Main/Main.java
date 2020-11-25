d7fd35ae021a21a8c9a2dcaef2722119a08da139
Moussa Dastgheib <moses@mdast.me>
2020-11-25 13:53:08 -0600

    Cleaned up master branch.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,20 +1,27 @@
 package Main;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        AnchorPane root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Sorting Visualizer");
-        primaryStage.setScene(new Scene(root, 800, 600));
+
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/Style/fxmls/main.fxml"));
         primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
 
     }
